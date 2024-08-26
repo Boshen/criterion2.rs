fb254e40d87b800fd0893adb729bf92541bd6c4f
Boshen <boshenc@gmail.com>
2024-08-26 16:45:04 +0800

    fix integration tests not being run

--- tests/criterion_tests.rs
+++ tests/criterion_tests.rs
@@ -7,10 +7,7 @@ use std::{
     time::{Duration, SystemTime},
 };
 
-use criterion::{
-    criterion_group, criterion_main, profiler::Profiler, BatchSize, BenchmarkFilter, BenchmarkId,
-    Criterion,
-};
+use criterion::{profiler::Profiler, BatchSize, BenchmarkFilter, BenchmarkId, Criterion};
 use serde_json::value::Value;
 use tempfile::{tempdir, TempDir};
 use walkdir::WalkDir;
@@ -327,66 +324,6 @@ fn test_criterion_doesnt_panic_if_measured_time_is_zero() {
     let dir = temp_dir();
     let mut c = short_benchmark(&dir);
     c.bench_function("zero_time", |bencher| bencher.iter_custom(|_iters| Duration::new(0, 0)));
-}
-
-mod macros {
-    use super::{criterion_group, criterion_main, Criterion};
-
-    #[test]
-    #[should_panic(expected = "group executed")]
-    fn criterion_main() {
-        fn group() {}
-        fn group2() {
-            panic!("group executed");
-        }
-
-        criterion_main!(group, group2);
-
-        main();
-    }
-
-    #[test]
-    fn criterion_main_trailing_comma() {
-        // make this a compile-only check
-        // as the second logger initialization causes panic
-        #[allow(dead_code)]
-        fn group() {}
-        #[allow(dead_code)]
-        fn group2() {}
-
-        criterion_main!(group, group2,);
-
-        // silence dead_code warning
-        if false {
-            main()
-        }
-    }
-
-    #[test]
-    #[should_panic(expected = "group executed")]
-    fn criterion_group() {
-        fn group(_crit: &mut Criterion) {}
-        fn group2(_crit: &mut Criterion) {
-            panic!("group executed");
-        }
-
-        criterion_group!(test_group, group, group2);
-
-        test_group();
-    }
-
-    #[test]
-    #[should_panic(expected = "group executed")]
-    fn criterion_group_trailing_comma() {
-        fn group(_crit: &mut Criterion) {}
-        fn group2(_crit: &mut Criterion) {
-            panic!("group executed");
-        }
-
-        criterion_group!(test_group, group, group2,);
-
-        test_group();
-    }
 }
 
 struct TestProfiler {
