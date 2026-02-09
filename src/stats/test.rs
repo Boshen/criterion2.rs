3c951aac82908db93e1961892b2cd6779ceca333
Boshen <boshenc@gmail.com>
2026-02-09 16:38:53 +0800

    chore: minimize rand dev features
    Disable default rand features in dev-dependencies and keep only std_rng. Update test RNG setup to seed StdRng deterministically so sys_rng is not required.

--- src/stats/test.rs
+++ src/stats/test.rs
@@ -1,7 +1,7 @@
 use rand::{
     distr::{Distribution, StandardUniform},
-    prelude::*,
     rngs::StdRng,
+    RngExt, SeedableRng,
 };
 
 pub fn vec<T>(size: usize, start: usize) -> Option<Vec<T>>
@@ -9,7 +9,8 @@ where
     StandardUniform: Distribution<T>,
 {
     if size > start + 2 {
-        let mut rng = StdRng::from_os_rng();
+        let seed = ((start as u64) << 32) ^ (size as u64);
+        let mut rng = StdRng::seed_from_u64(seed);
 
         Some((0..size).map(|_| rng.random()).collect())
     } else {
